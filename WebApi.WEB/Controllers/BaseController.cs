aaceeb7e948c99596fd80ad2d06e2bae77915abe
a.vysotski <a.vysotski.vironit@vironit.ru>
2022-02-07 13:55:04 +0300

    Init: created an empty project, added three layer architecture and added git ignore file

---
+++ WebApi.WEB/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.WEB.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+}
